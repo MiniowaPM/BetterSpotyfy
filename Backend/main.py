58937a6de26340429e7d702b635d3ee7d86532f7
unknown <31001@s.pm.szczecin.pl>
2024-11-02 11:55:54 +0100

    Updated orm models, improving endpoints

--- Backend/main.py
+++ Backend/main.py
@@ -1,6 +1,6 @@
 from fastapi import FastAPI, HTTPException, Depends, status
-from pydantic import BaseModel
-from typing import Annotated
+from pydantic import BaseModel, Field
+from typing import Annotated, List
 import models
 from database import engine, SessionLocal
 from sqlalchemy.orm import Session
@@ -8,7 +8,6 @@ from sqlalchemy.orm import Session
 app = FastAPI()
 
 models.Base.metadata.create_all(bind=engine)
-
 
 # Base Models / Schemas
 class AlbumBase(BaseModel):
@@ -23,12 +22,13 @@ class SongBase(BaseModel):
 
 class UserBase(BaseModel):
     username: str
-    firstname: str
-    lastname: str
+    first_name: str
+    last_name: str
     email: str
-    gender: str
-    password: str
-
+    gender: int
+    password_hash: str
+    wallet: str = Field(default="", nullable=True)
+    is_admin: bool = False
 
 # utility functions
 def get_db():
@@ -41,42 +41,62 @@ def get_db():
 db_dependency = Annotated[Session, Depends(get_db)]
 
 # Endpoints
-@app.post("/users", status_code=status.HTTP_201_CREATED)
+@app.post("/api/user/", status_code=status.HTTP_201_CREATED, response_model=UserBase)
 async def create_user(user: UserBase, db: db_dependency):
-    db_user = models.User(**user.dict())
-    db.add(user)
+    db_user = models.User(**user.model_dump())
+    db.add(db_user)
     db.commit()
+    db.refresh(db_user)
+    return db_user
 
-@app.get("/user/{user_id}", status_code=status.HTTP_200_OK)
-async def get_user(user: UserBase, db: db_dependency):
+@app.get("/api/user/{user_id}", status_code=status.HTTP_200_OK, response_model=UserBase)
+async def get_user(user_id: int, db: db_dependency):
     user = db.query(models.User).filter(models.User.id == user_id).first()
     if user is None:
         raise HTTPException(status_code=404, detail="User not found")
     return user
 
-@app.post("/album", status_code=status.HTTP_201_CREATED)
+@app.get("/api/users/", status_code=status.HTTP_200_OK, response_model=List[UserBase])
+async def get_users(db: db_dependency):
+    users = db.query(models.User).all()
+    return users
+
+@app.post("/api/album/", status_code=status.HTTP_201_CREATED, response_model=AlbumBase)
 async def create_album(album: AlbumBase, db: db_dependency):
     db_album = models.Album(**album.dict())
-    db.add(album)
+    db.add(db_album)
     db.commit()
+    db.refresh(db_album)
+    return db_album
 
-@app.get("/album/{album_id}", status_code=status.HTTP_200_OK)
-async def get_album(user: AlbumBase, db: db_dependency):
+@app.get("/api/album/{album_id}", status_code=status.HTTP_200_OK, response_model=AlbumBase)
+async def get_album(album_id: int, db: db_dependency):
     album = db.query(models.Album).filter(models.Album.id == album_id).first()
     if album is None:
-        raise HTTPException(status_code=404, detail="User not found")
+        raise HTTPException(status_code=404, detail="Album not found")
     return album
 
-@app.post("/album={album_id}/song", status_code=status.HTTP_201_CREATED)
-async def create_song(song: SongBase, db: db_dependency):
-    db_songs = models.Song(**song.dict())
-    db.add(song)
-    db.commit()
+@app.get("/api/albums/", status_code=status.HTTP_200_OK, response_model=List[AlbumBase])
+async def get_albums(db: db_dependency):
+    albums = db.query(models.Album).all()
+    return albums
 
-@app.get("/song={song_id}", status_code=status.HTTP_200_OK)
-async def get_song(song: SongBase, db: db_dependency):
+@app.post("/api/album/{album_id}/song", status_code=status.HTTP_201_CREATED, response_model=SongBase)
+async def create_song(album_id: int, song: SongBase, db: db_dependency):
+    db_song = models.Song(**song.dict(), album_fk=album_id)
+    db.add(db_song)
+    db.commit()
+    db.refresh(db_song)
+    return db_song
+
+@app.get("/api/song={song_id}", status_code=status.HTTP_200_OK)
+async def get_song(song_id: int, db: db_dependency):
     song = db.query(models.Song).filter(models.Song.id == song_id).first()
-    album = db.query(models.Album).filter(models.Album.id == album_id).first()
-    if song and album is None:
-        raise HTTPException(status_code=404, detail="User not found")
+    if song is None:
+        raise HTTPException(status_code=404, detail="Song not found")
     return song
+
+@app.get("/api/songs/", status_code=status.HTTP_200_OK, response_model=List[SongBase])
+async def get_song(db: db_dependency):
+    songs = db.query(models.Song).all()
+    return songs
--- Backend/models.py
+++ Backend/models.py
@@ -1,5 +1,6 @@
-from sqlalchemy import Boolean, Column, Integer, String, ForeignKey
-from sqlalchemy.orm import relationship 
+from sqlalchemy import Boolean, Column, Integer, String, ForeignKey, DateTime
+from sqlalchemy.orm import relationship
+from sqlalchemy.sql import func
 from database import Base
 
 class Gender(Base):
@@ -14,25 +15,26 @@ class Genre(Base):
     id = Column(Integer, primary_key=True, index=True)
     genre = Column(String(50), unique=True)
 
-class User(Base):
-    __tablename__ = 'users'
-
-    id = Column(Integer, primary_key=True, index=True)
-    username = Column(String(50), unique=True)
-    first_name = Column(String(50), unique=False)
-    last_name = Column(String(50), unique=False)
-    email = Column(String(50), unique=True)
-    gender = Column(Integer, ForeignKey("gender.id"))
-    password_hash = Column(String(50), unique=True)
-    wallet = Column(String(50), unique=False)
-
 class Songs_owned(Base):
     __tablename__ = 'songs_owned'
     
     id = Column(Integer, primary_key=True, index=True)
-    # song_fk =
-    # user_fk =
-    # creadte_date =
+    song_fk = Column(Integer, ForeignKey('songs.id'))
+    user_fk = Column(Integer, ForeignKey('users.id'))
+    create_date = Column(DateTime(timezone=True), server_default=func.now())
+
+class User(Base):
+    __tablename__ = 'users'
+
+    id = Column(Integer, primary_key=True, index=True, autoincrement=True)
+    username = Column(String(50), unique=True)
+    first_name = Column(String(50), unique=False)
+    last_name = Column(String(50), unique=False)
+    email = Column(String(100), unique=True)
+    gender = Column(Integer, ForeignKey('gender.id'))
+    password_hash = Column(String(128), unique=False)
+    wallet = Column(String(50), unique=False, nullable=True)
+    is_admin = Column(Boolean, unique=False, default=False)
 
 class Album(Base):
     __tablename__ = 'albums'
@@ -40,7 +42,7 @@ class Album(Base):
     id = Column(Integer, primary_key=True, index=True)
     title = Column(String(50), unique=True)
     description = Column(String(500), unique=False)
-    # genre = Column(String(50), unique=False) <- FK to a genre table
+    genre = Column(Integer, ForeignKey('genres.id'))
 
 class Song(Base):
     __tablename__ = 'songs'
@@ -48,5 +50,5 @@ class Song(Base):
     id = Column(Integer, primary_key=True, index=True)
     title = Column(String(50), unique=True)
     description = Column(String(500), unique=False)
-    genre = Column(String(50), unique=False)
-    # album_fk = Column(Integer)
+    genre = Column(Integer, ForeignKey('genres.id'))
+    album_fk = Column(Integer, ForeignKey('albums.id'))
